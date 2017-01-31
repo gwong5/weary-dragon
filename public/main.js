68f38275758e8030d4ef9bb8e5094b1ec1459418
Greg <kousagi1012@yahoo.com>
2017-01-31 09:23:31 -0800

    complete number guess game except for exit feature

--- public/main.js
+++ public/main.js
@@ -4,10 +4,9 @@ let counter = 0
 
 const guessNumber = (input) => {
   let inputValue = input.value
+  console.log(randomNumber);
   if (isNaN(inputValue) || inputValue < 1 || inputValue > 100) alert('Must be a number between 1 and 100')
-  if (inputValue === randomNumber) {
-    alert('You Win!')
-  }
+  if (inputValue == randomNumber) alert('You Win!')
   if (inputValue > randomNumber && inputValue <= 100) {
     alert('Guess Lower')
     counter++
