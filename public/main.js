c6e4839a1516e5d4f1fcd6ac45289c39ea33ef37
Greg <kousagi1012@yahoo.com>
2017-01-30 17:29:18 -0800

    finished number guessing game minus exit spec

---
+++ public/main.js
@@ -0,0 +1,27 @@
+const randomNumber = Math.floor(Math.random() * (100) + 1)
+
+let counter = 0
+
+const guessNumber = (input) => {
+  let inputValue = input.value
+  if (isNaN(inputValue) || inputValue < 1 || inputValue > 100) alert('Must be a number between 1 and 100')
+  if (inputValue === randomNumber) {
+    alert('You Win!')
+  }
+  if (inputValue > randomNumber && inputValue <= 100) {
+    alert('Guess Lower')
+    counter++
+  }
+  if (inputValue < randomNumber && inputValue >= 0) {
+    alert('Guess Higher')
+    counter++
+  }
+  document.getElementById("guesses").innerHTML = "Guesses so far: " + counter
+}
+  //user can terminate by typing exit
+
+//Scrab-Bag
+//scrabble game: has "bag" of 100 tiles
+//has set number of frequency of each letter
+//inputs will all be uppercase
+//
